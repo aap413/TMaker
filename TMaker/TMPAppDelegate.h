983f7fcab397a2068dbf0850e14b9a7b84e028c0
Manoj Bidnurkar <mxb100@hotmail.com>
2014-02-09 15:14:47 -0600

    MB initial

---
+++ TMaker/TMPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TMPAppDelegate.h
+//  TMaker
+//
+//  Created by MANOJ BIDNURKAR on 2/9/14.
+//  Copyright (c) 2014 TM Pro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TMPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TMaker/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TMaker
+//
+//  Created by MANOJ BIDNURKAR on 2/9/14.
+//  Copyright (c) 2014 TM Pro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TMPAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TMPAppDelegate class]));
+    }
+}
